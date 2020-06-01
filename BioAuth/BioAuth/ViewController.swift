d7a2370de10a4f5b32853d7919872fd2c15a9930
Amr Hesham <amr.hesham@fawry.com>
2020-06-01 12:31:07 +0200

    evalutaing policy

--- BioAuth/BioAuth/ViewController.swift
+++ BioAuth/BioAuth/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
-
+import LocalAuthentication
 class ViewController: UIViewController {
-
+    var context = LAContext()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        context.localizedCancelTitle = "End Session"
+        context.localizedFallbackTitle = "Use Passcode"
+        context.localizedReason = "The app needs your authentaction."
+        
+        // if the user has unlocked the device using touch ID already .. my app can ignore authenticating him again for some duration .. use the following property to set this duration
+        context.touchIDAuthenticationAllowableReuseDuration = LATouchIDAuthenticationMaximumAllowableReuseDuration
+        evaluatePolicy()
     }
 
+    func evaluatePolicy(){
+        var errorCanEval:NSError?
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &errorCanEval) {
+            
+            switch context.biometryType {
+            case .faceID:
+                print("we are going to use face id")
+            case .touchID:
+                print("we are going to use touch id")
+            case .none:
+                print("none.")
+           
+            @unknown default:
+                print("unknown")
+            }
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "can't recognize the face") { (success, error) in
+                if (success){
+                    print("evaluted successfully")
+                }else{
+                    if let err = error{
+                        let evalErro = LAError(_nsError: error as! NSError)
+                        switch evalErro.code {
+                        case LAError.Code.userCancel:
+                            print("User has canceled")
+                        case LAError.Code.userFallback:
+                            print("fallback")
+                        case LAError.Code.authenticationFailed:
+                            print("failed")
+                        default:
+                            print("general error")
+                        }
+                    }
+                }
+            }
+        }else{
+            print("can't evaluate")
+            print(errorCanEval?.localizedDescription ?? "error has no description")
+        }
+    }
 
 }
 
